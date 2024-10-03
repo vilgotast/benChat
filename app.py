4fce43c75eb8ad47033a916c5d8c1c0f9f3be503
Vilgot <vilgot00@gmail.com>
2024-10-03 17:50:25 +0200

    fixed port issue

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ def generate():
     return jsonify({"response": ai_response})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
